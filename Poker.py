68d10f238ed328a1d886d0c556cf403c2bf88650
Audrey McNay <amcnay@utexas.edu>
2018-02-10 15:43:15 -0600

    fix play method

--- Poker.py
+++ Poker.py
@@ -112,46 +112,43 @@ class Poker (object):
 
     for i in range (len(self.players)):
       if is_royal(self.players[i]) != 0:
-        points_hand[i] = is_royal(self.players[i])
+        points_hand.append(is_royal(self.players[i]))
         print("Player", str(i + 1)+ ": Royal Flush")
       elif is_straight_flush(self.players[i]) != 0:
-        points_hand[i] = is_straight_flush(self.players[i])
+        points_hand.append(is_straight_flush(self.players[i]))
         print("Player", str(i + 1)+ ": Straight Flush")
       elif is_four_kind(self.players[i]) != 0:
-        points_hand[i] = is_four_kind[self.players[i]]
+        points_hand.append(is_four_kind(self.players[i]))
         print("Player", str(i + 1)+ ": Four of a Kind")
       elif is_full_house(self.players[i]) != 0:
-        points_hand[i] = is_full_house(self.players[i])
+        points_hand.append(is_full_house(self.players[i]))
         print("Player", str(i + 1)+ ": Full House")
       elif is_flush(self.players[i]) != 0:
-        points_hand[i] = is_flush(self.players[i])
+        points_hand.append(is_flush(self.players[i]))
         print("Player", str(i + 1)+ ": Flush")
       elif is_straight(self.players[i]) != 0:
-        points_hand[i] = is_straight(self.players[i])
+        points_hand.append(is_straight(self.players[i]))
         print("Player", str(i + 1)+ ": Straight")
       elif is_three_kind(self.players[i]) != 0:
-        points_hand[i] = is_three_kind(self.players[i])
+        points_hand.append(is_three_kind(self.players[i]))
         print("Player", str(i + 1)+ ": Three of a Kind")
       elif is_two_pair(self.players[i]) != 0:
-        points_hand[i] = is_two_pair(self.players[i])
+        points_hand.append(is_two_pair(self.players[i]))
         print("Player", str(i + 1)+ ": Two Pair")
       else:
-        points_hand[i] = is_high_card(self.players[i])
+        points_hand.append(is_high_card(self.players[i]))
         print("Player", str(i + 1)+ ": High Card")
     # determine winner and print
-    max_ind = []
-    max_val = points_hand[i]
-    for i in len(points_hand):
-      if points_hand[i] >= max_val:
-        max_val = points_hand[i]
-    for i in len(points_hand):
+    max_players = [] # list of players with max total points value
+    max_val = max(points_hand)
+    for i in range len(points_hand):
       if points_hand[i] == max_val:
-        max_ind.append(i)
-    if len(max_ind) == 1:
-      print("Player", str(max_ind[0]), "wins.")
+        max_players.append(i+1)
+    if len(max_players) == 1:
+      print("Player", str(max_players[0]), "wins.")
     else:
-      for i in len(max_ind):
-        print("Player", str(max_ind[i], "ties."))
+      for i in len(max_players):
+        print("Player", str(max_players[i], "ties."))
 
 
   # determine if a hand is a royal flush
@@ -161,7 +158,7 @@ class Poker (object):
       same_suit = same_suit and (hand[i].suit == hand[i + 1].suit)
 
     if (not same_suit):
-      return False
+      return 0
 
     rank_order = True
     for i in range (len(hand)):
